71a0ef4c7953ccb70cd389c3473760372ae66c73
Pedro Silva <pedro.rls93@gmail.com>
2019-02-21 15:40:34 -0300

    separated in several components and get user location

--- src/components/Season.jsx
+++ src/components/Season.jsx
@@ -1,8 +1,42 @@
 import React from 'react';
 
-export default class Season extends React.Component() {
+export default class Season extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      lat: null,
+      lon: null,
+      error: null
+    };
   }
+
+  componentDidMount() {
+    this.getPosition();
+  }
+
+
+  getPosition() {
+    window.navigator.geolocation.getCurrentPosition(
+      position => this.setState({
+        lat: position.coords.latitude,
+        lon: position.coords.longitude,
+      }),
+      err => this.setState({ error: err }),
+    )
+  }
+
+  render() {
+    const { lat, lon } = this.state;
+    return (
+      <div>
+        <h1>Your Current Position</h1>
+        <p>
+          Latitude: { lat }
+        </p>
+        <p>
+          Longitude: { lon }
+        </p>
+      </div>
+    );
+  };
 }
